9f07bfa8810793887f3bd239db8fb5f95a5aae54
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-06 06:10:17 +0000

    [update]mypage_edit

--- app/controllers/users/mypages_controller.rb
+++ app/controllers/users/mypages_controller.rb
@@ -1,13 +1,25 @@
 class Users::MypagesController < ApplicationController
   def edit
+    # 現在ログインしているの情報を取得
+    @user = User.find(current_user.id)
   end
 
   def update
+    # 現在ログインしているの情報を取得
+    @user = User.find(current_user.id)
+    @user.update(user_params)
+    redirect_to rooms_path
   end
 
   def unsubscribe
   end
 
   def withdraw
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image)
   end
 end

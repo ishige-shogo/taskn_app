e08ace85c9f8f8b42a8073bd0e8e4980962edf8a
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-06 00:57:04 +0000

    [add]room_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_004019) do
+ActiveRecord::Schema.define(version: 2021_01_06_005026) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 2021_01_06_004019) do
     t.string "title"
     t.text "body"
     t.boolean "is_read", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.string "encrypted_roompass"
+    t.string "encrypted_roompass_iv"
+    t.date "deadline"
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

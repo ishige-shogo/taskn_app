d834674c824b5808d7a9cfca916ff15a3702b5a5
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-06 14:29:43 +0000

    [update]user_withdraw

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :reject_user, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -19,6 +19,22 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # protected
+
+  #ログイン後ルーム一覧に遷移する処理
+  def after_sign_in_path_for(resource)
+    rooms_path
+  end
+
+  #退会した利用者がログインできないようにする処理
+  def reject_user
+    user = User.find_by(email: params[:user][:email].downcase)
+    if user
+      if (user.valid_password?(params[:user][:password]) && (user.active_for_authentication? != true))
+        flash[:notice] = "退会済です"
+        redirect_to new_user_session_path
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params

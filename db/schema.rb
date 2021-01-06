af8d41fd6354f36509d7586022ce022b3084fe30
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-06 01:15:05 +0000

    [add]memo_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_011221) do
+ActiveRecord::Schema.define(version: 2021_01_06_011353) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2021_01_06_011221) do
   end
 
   create_table "logs", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "memos", force: :cascade do |t|
     t.integer "user_id"
     t.integer "room_id"
     t.string "body"

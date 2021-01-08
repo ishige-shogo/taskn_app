7fd550b65060d8d67688d58e6636f781d658efbf
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-08 12:07:45 +0000

    [add]analysis_member

--- app/controllers/users/analysis_controller.rb
+++ app/controllers/users/analysis_controller.rb
@@ -1,6 +1,8 @@
 class Users::AnalysisController < ApplicationController
   def show
 
+    @room_users = RoomUser.where(room_id: current_user.present_room)
+
     # 未着手(TODOリストに残っているタスク)
     lists = List.where(room_id: current_user.present_room)
     # 実行中のタスク
@@ -17,5 +19,9 @@ class Users::AnalysisController < ApplicationController
   end
 
   def edit
+    @room_user = RoomUser.find(params[:id])
+    @on_task = Task.where(room_id: current_user.present_room, user_id: @room_user.user_id, is_finished: false)
+    @logs = Log.where(room_id: current_user.present_room, user_id: @room_user.user_id)
+    @finished_tasks = Task.where(room_id: current_user.present_room, is_finished: true, user_id: @room_user.user_id)
   end
 end

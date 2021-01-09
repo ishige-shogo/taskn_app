f7d1deaa44e1070e4d1588fa96196bd01b5aeb5d
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-09 11:28:34 +0000

    [add]admin_authenticate

--- app/controllers/admins/analysis_controller.rb
+++ app/controllers/admins/analysis_controller.rb
@@ -1,4 +1,6 @@
 class Admins::AnalysisController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @users = User.group_by_day(:created_at).count
     @rooms = Room.group_by_day(:created_at).count
--- app/controllers/admins/rooms_controller.rb
+++ app/controllers/admins/rooms_controller.rb
@@ -1,4 +1,5 @@
 class Admins::RoomsController < ApplicationController
+  before_action :authenticate_admin!
   def index
     @rooms = Room.all
   end

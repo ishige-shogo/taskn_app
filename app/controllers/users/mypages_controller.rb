4000dbecbd585d5aa7db586cec9e825e6bab9954
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-09 10:01:18 +0000

    [update]mypage_room

--- app/controllers/users/mypages_controller.rb
+++ app/controllers/users/mypages_controller.rb
@@ -1,6 +1,7 @@
 class Users::MypagesController < ApplicationController
   def edit
     @user = User.find(current_user.id)
+    @user_rooms = RoomUser.where(user_id: current_user.id)
   end
 
   def update

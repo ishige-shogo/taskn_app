52c8377dabcf8e4dbb83ee64f195874e6a38cdb0
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-10 04:30:40 +0000

    [add]guest_login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,9 +5,12 @@ class Users::SessionsController < Devise::SessionsController
   before_action :authenticate_user!
   before_action :reject_user, only: [:create]
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    @guest_name = ENV["GUEST_NAME"]
+    @guest_email = ENV["GUEST_EMAIL"]
+    @guest_password = ENV["GUEST_PASSWORD"]
+    super
+  end
 
   # POST /resource/sign_in
   # def create

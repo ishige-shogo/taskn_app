169c43817c266249a56546eeedcf7816656661b5
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-12 06:50:31 +0000

    [design]home_top_explain

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,3 +39,22 @@ $(function(){
     readURL(this);
   });
 });
+
+//ページが浮かび上がる機能
+
+function showElementAnimation() {
+
+  var element = document.getElementsByClassName('js-animation');
+  if(!element) return;
+
+  var scrollY = window.pageYOffset;
+  var windowH = window.innerHeight;
+
+  for(var i=0;i<element.length;i++) { var elemClientRect = element[i].getBoundingClientRect(); var elemY = scrollY + elemClientRect.top; if(scrollY + windowH  > elemY) {
+      element[i].classList.add('is-show');
+    }
+  }
+}
+showElementAnimation();
+window.addEventListener('scroll', showElementAnimation);
+
